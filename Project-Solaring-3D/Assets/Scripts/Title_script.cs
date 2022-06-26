b34f17090c521de7d5edbbb0f505c6533e06b94e
Iirl <102848236+Iirl@users.noreply.github.com>
2022-06-26 18:48:47 +0800

    220626-i1: Title function test
    選單的控制意外複雜，滑鼠跟鍵盤只能分開處理的樣子(?)

--- Project-Solaring-3D/Assets/Scripts/Title_script.cs
+++ Project-Solaring-3D/Assets/Scripts/Title_script.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 /// <summary>
@@ -10,19 +11,55 @@ public class Title_script : MonoBehaviour
     [SerializeField, Header("Object Obtain")]
     private Camera cam;
     [SerializeField]
+    private GameObject btn_str, btn_opt, btn_open;
+    [SerializeField]
     private RectTransform bg_space, bg_earth, cvs_rect;
+    private AudioSource audioSrc;
+    [SerializeField]
+    private AudioClip acp_down;
     [SerializeField, Header("Property Adjust")]
-    private float bg_move_speed = 0.01f;
-    private bool bg_move = true;
-    
+    private float bg_move_speed = 0.5f;
+    // 其他屬性(欄位)
+    private bool bg_move = true, reload_scene = false;
+
     #endregion
 
     #region 基本功能
-
-    private void Move2Center(RectTransform rts, float end) {
+    /// <summary>
+    /// 調整選單的垂直位置
+    /// </summary>
+    /// <param name="rts">傳入的 Rect 元件</param>
+    /// <param name="end">移動結束點</param>
+    private void Move2Center(RectTransform rts, float end)
+    {
         float y = rts.position.y;
-        if (Mathf.Ceil(y) != Mathf.Ceil(end)) rts.Translate(new Vector2(0,end-y)*Time.deltaTime);
+        if (Mathf.Ceil(y) != Mathf.Ceil(end))
+        {
+            rts.Translate(new Vector2(0, end - y) * Time.deltaTime * bg_move_speed);
+        }
+        else
+        {
+            bg_move = false;
+            reload_scene = true;
+        }
 
+    }
+    /// <summary>
+    /// 重讀場景
+    /// </summary>
+    private void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    /// <summary>
+    /// 選單事件：第一層->第二層
+    /// </summary>
+    public void Menu1to2()
+    {
+        btn_str.SetActive(true);
+        btn_opt.SetActive(true);
+        btn_open.SetActive(false);
+        audioSrc.PlayOneShot(acp_down);
     }
 
     #endregion
@@ -30,19 +67,27 @@ public class Title_script : MonoBehaviour
     #region 觸發事件
     private void Awake()
     {
-        
+        audioSrc = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+
     }
 
     private void Start()
     {
-        
 
     }
 
     private void FixedUpdate()
     {
-        if (bg_space != null && bg_move) Move2Center(bg_space, cvs_rect.position.y/2);
-        if (bg_earth != null && bg_move) Move2Center(bg_earth, cvs_rect.position.y/4);
+        if (bg_space != null && bg_move) Move2Center(bg_space, cvs_rect.position.y / 2);
+        if (bg_earth != null && bg_move) Move2Center(bg_earth, cvs_rect.position.y / 4);
     }
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Bbutton") > 0 && reload_scene) ReloadCurrentScene(); // 重讀場景
+        if (Input.anyKeyDown && btn_open.activeSelf) Menu1to2(); // 鍵盤觸發進入下一層選單事件
+
+    }
+
+
     #endregion
 }
