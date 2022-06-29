2d9f274e70babc2bfba1ae937b9d5bba7bc9106b
Iirl <102848236+Iirl@users.noreply.github.com>
2022-06-29 16:26:59 +0800

    220629: bug fix

--- Project-Solaring-3D/Assets/Scripts/Rocket_Controll.cs
+++ Project-Solaring-3D/Assets/Scripts/Rocket_Controll.cs
@@ -13,8 +13,8 @@ public class Rocket_Controll : MonoBehaviour
     #region #序列化屬性
     [SerializeField, Header("燃料"),Range(100,200)]
     float fuel=100;
-    [SerializeField, Header("移動速度"), Range(8,20)]
-    float speed_v=8;
+    [SerializeField, Header("移動速度"), Range(0.1f,2f)]
+    float speed_v= 0.8f;
     [SerializeField, Header("移動加速度"), Range(0.2f,1.5f)]
     float speed_a=0.2f;
     #endregion
@@ -33,12 +33,18 @@ public class Rocket_Controll : MonoBehaviour
     {
         float aSpeed = ( Input.GetKeyDown(KeyCode.F)) ? speed_a : 0;
         if (Mathf.Abs(horizon) < 0.005f) horizon = 0;
-        if (Mathf.Abs(vertial) < 0.005f || !particle_fire.isPlaying) vertial = 0;
+        if (Mathf.Abs(vertial) < 0.005f) vertial = 0;
         float xSpeed = (speed_v * horizon) * Time.deltaTime;
         float ySpeed = (speed_v * vertial) * Time.deltaTime;
         Vector3 v3 = new Vector3(xSpeed , ySpeed, 0);
+        rb_Rocket.transform.Rotate(transform.rotation.x, transform.rotation.y, 0);
         rb_Rocket.velocity += v3;
 
+        //print($"H:{horizon}; V:{vertial}; Fire{particle_fire.isPlaying}");        
+        //print($"H+V:{Mathf.Abs(horizon) + Mathf.Abs(vertial)};");        
+        //if (Mathf.Abs(horizon) == 0 && Mathf.Abs(vertial) == 0) ignix_fire(false);
+        particle_fire.transform.localScale = new Vector2(Mathf.Abs(Mathf.Pow(1.2f, horizon))-0.5f, Mathf.Abs(Mathf.Pow(4,vertial)) - 1);
+        //else  ignix_fire(true);
 
     }
     /// <summary>
@@ -47,11 +53,16 @@ public class Rocket_Controll : MonoBehaviour
     /// <param name="isFire">判斷是否啟動引擎</param>
     private void ignix_fire(bool isFire)
     {
-        if (isFire) particle_fire.Stop();
-        else particle_fire.Play();
-    }
-    private void VisiableWall()
-    {
+        if (isFire)
+        {
+            //particle_fire.gameObject.SetActive(true);
+            particle_fire.Play();
+        }
+        else
+        {
+            //particle_fire.gameObject.SetActive(false);
+            particle_fire.Pause();
+        }
     }
 
     #endregion
@@ -61,6 +72,7 @@ public class Rocket_Controll : MonoBehaviour
     {
         particle_fire = GetComponentInChildren<ParticleSystem>();
         rb_Rocket = GetComponent<Rigidbody>();
+        ignix_fire(true);
     }
     void Start()
     {
@@ -72,7 +84,7 @@ public class Rocket_Controll : MonoBehaviour
     void Update()
     {
         MoveControll(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (Input.GetKey(KeyCode.Space)) ignix_fire(particle_fire.isPlaying);
+        
         //VisiableWall();
     }
     private void FixedUpdate()
