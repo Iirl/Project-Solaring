e7c4452715e0512d363e44059b94f7fbd4c9de4c
Iirl <102848236+Iirl@users.noreply.github.com>
2022-06-30 16:32:32 +0800

    220630: move and fire fix

--- Project-Solaring-3D/Assets/Scripts/Rocket_Controll.cs
+++ Project-Solaring-3D/Assets/Scripts/Rocket_Controll.cs
@@ -13,8 +13,8 @@ public class Rocket_Controll : MonoBehaviour
     #region #序列化屬性
     [SerializeField, Header("燃料"),Range(100,200)]
     float fuel=100;
-    [SerializeField, Header("移動速度"), Range(0.1f,2f)]
-    float speed_v= 0.8f;
+    [SerializeField, Header("移動速度"), Range(2.0f,12f)]
+    float speed_v= 4f;
     [SerializeField, Header("移動加速度"), Range(0.2f,1.5f)]
     float speed_a=0.2f;
     #endregion
@@ -29,22 +29,35 @@ public class Rocket_Controll : MonoBehaviour
     /// </summary>
     /// <param name="horizon">水平移動向量</param>
     /// <param name="vertial">垂直移動向量</param>
-    private void MoveControll(float horizon, float vertial)
+    private void MoveControll(float horizon, float vertial, bool boost)
     {
-        float aSpeed = ( Input.GetKeyDown(KeyCode.F)) ? speed_a : 0;
+        // 移動控制
         if (Mathf.Abs(horizon) < 0.005f) horizon = 0;
         if (Mathf.Abs(vertial) < 0.005f) vertial = 0;
         float xSpeed = (speed_v * horizon) * Time.deltaTime;
         float ySpeed = (speed_v * vertial) * Time.deltaTime;
-        Vector3 v3 = new Vector3(xSpeed , ySpeed, 0);
-        rb_Rocket.transform.Rotate(transform.rotation.x, transform.rotation.y, 0);
+        float aSpeed = speed_a * 1000;
+        Vector3 v3 = new Vector3(xSpeed , ySpeed, 0); 
+        Vector3 r3 = new Vector3(horizon * aSpeed, vertial * aSpeed, 0);
         rb_Rocket.velocity += v3;
-
+        if (boost) rb_Rocket.AddForce(r3);
+        // 翻轉回復
+        rb_Rocket.transform.Rotate(transform.rotation.x, transform.rotation.y, 0);
         //print($"H:{horizon}; V:{vertial}; Fire{particle_fire.isPlaying}");        
-        //print($"H+V:{Mathf.Abs(horizon) + Mathf.Abs(vertial)};");        
-        //if (Mathf.Abs(horizon) == 0 && Mathf.Abs(vertial) == 0) ignix_fire(false);
-        particle_fire.transform.localScale = new Vector2(Mathf.Abs(Mathf.Pow(1.2f, horizon))-0.5f, Mathf.Abs(Mathf.Pow(4,vertial)) - 1);
-        //else  ignix_fire(true);
+        //print($"H+V:{Mathf.Abs(horizon) + Mathf.Abs(vertial)};");
+        // 火焰控制
+        if (particle_fire != null) { 
+            float xFire = Mathf.Abs(Mathf.Pow(1f, horizon)) - 0.5f;
+            float yFire=0;
+            if (vertial > 0)  yFire = Mathf.Pow(4, vertial) - 1;
+            else if (vertial <0) yFire = Mathf.Abs(Mathf.Pow(1, vertial));
+            if (boost) yFire += 1;
+            particle_fire.transform.localScale = new Vector2(xFire, yFire);
+            if (particle_fire.transform.localScale.x < 0.6f && particle_fire.transform.localScale.y < 1) ignix_fire(false);
+            else  ignix_fire(true);
+        }
+
+        print(r3);
 
     }
     /// <summary>
@@ -83,9 +96,8 @@ public class Rocket_Controll : MonoBehaviour
     /// </summary>
     void Update()
     {
-        MoveControll(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        MoveControll(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetKey(KeyCode.Space));
         
-        //VisiableWall();
     }
     private void FixedUpdate()
     {
