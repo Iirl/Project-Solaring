982d24329c2d89d9b586e8c02aea5ec36f46caef
Iirl <102848236+Iirl@users.noreply.github.com>
2022-07-14 23:53:50 +0800

    220714: Basic Space Spine & Background Loop
    完成基本四方旋轉空間；背景動畫重複撥放
    注意：RtlLookupFunctionEntry returned NULL function. Aborting stack walk.
    請在更新檔案之前確定沒有錯誤在更新...

--- Project-Solaring-3D/Assets/Scripts/Space_Controll.cs
+++ Project-Solaring-3D/Assets/Scripts/Space_Controll.cs
@@ -24,69 +24,87 @@ namespace solar_a
         #endregion
 
         #region 90度固定旋轉空間
+        /// <summary>
+        /// 方向(轉向)判定
+        /// </summary>
         private void DirectCheck()
         {
             if (!rotated)
             {
-                if (rot_left)
-                {
-                    Coordinate = PositionCheck(1); //Q
-
-                }
-                else if (rot_right)
-                {
-                    Coordinate = PositionCheck(-1); //E
-                }
                 if (rot_right || rot_left)
                 {
+                    Coordinate = PositionCheck(rot_left); //若要左轉為Q則放 Left ，反之則顛倒。
                     InvokeRepeating("Spine", 0, 0.05f);
                     rotated = true;
                 }
             }
-            else
-            {
-                print("還在旋轉");
-            }
+            else print("還在旋轉");
         }
-        private float PositionCheck(int direct)
+        /// <summary>
+        /// 位置判定
+        /// </summary>
+        /// <param name="direct">轉向指引，若為真則+90度；假則是-90度</param>
+        /// <returns></returns>
+        private float PositionCheck(bool direct)
         {
             float y_axis = Mathf.Floor(transform.eulerAngles.y); // 目前Y軸軸心位置
             int quadrant = (Int32.Parse(y_axis.ToString()) / 90);  //所在象限
-            if (quadrant == 0 && direct == -1) quadrant = 4;
-            float next_axis = (direct == 1) ? 90 * (quadrant + 1) : 90 * (quadrant-1); // 下一個Y軸座標
-            if (quadrant == 3 && direct == 1) next_axis %= 360;
-            print($"Now:{y_axis}, quadrant:{quadrant}");
-            print($"Next Y:{next_axis}");
+            if (quadrant == 0 && !direct) quadrant = 4;
+            float next_axis = (direct) ? 90 * (quadrant + 1) : 90 * (quadrant - 1); // 下一個Y軸座標
+            if (quadrant == 3 && direct) next_axis %= 360;
+            //print($"Now:{y_axis}, quadrant:{quadrant}");
+            //print($"Next Y:{next_axis}");
             return Mathf.Round(next_axis);
         }
+        /// <summary>
+        /// 旋轉程式
+        /// </summary>
         private void Spine()
         {
             float y_axis = Mathf.Floor(transform.eulerAngles.y); // 目前Y軸軸心位置
             float Distane2axis = (Mathf.DeltaAngle(y_axis, Coordinate)); // 到達下一個Y軸座標的距離
 
-            print(Distane2axis);
-            //print(Mathf.Pow(5, (Coordinate+1)/ (i+1)));
 
-            if (Mathf.Round(Distane2axis) != 0)
+            if (rotated)
             {
+                StopCheck();
                 float upper = 1;
                 if (upper < 0.01f) upper = 0.01f;
-                float iSpine = Mathf.Pow(5, upper);
-                //if (y_axis < Coordinate) iSpine *= -1;
+                float iSpine = Mathf.Pow(spine, upper);
+                // 收束方法
+                if (Mathf.Abs(Distane2axis) < 5) iSpine = 0.5f;
+                if (Mathf.Abs(Distane2axis) < 2) iSpine = 0.1f;
+
+                // 執行旋轉函式
                 if (rot_left) transform.Rotate(Vector3.up * iSpine);
-                if (rot_right) transform.Rotate(Vector3.down * iSpine);
-                if (iSpine == Mathf.Infinity) print(upper);
+                else if (rot_right) transform.Rotate(Vector3.down * iSpine);
+                //if (iSpine == Mathf.Infinity) print(upper);
             }
-            else
+        }
+        /// <summary>
+        /// 停止判定，採定點判斷，若無法準確到定點則可能無限旋轉。
+        /// </summary>
+        private void StopCheck()
+        {
+            float y_axis = Mathf.Floor(transform.eulerAngles.y); // 目前Y軸軸心位置
+            bool stop = !rotated;
+
+            switch (Mathf.Floor(y_axis))
+            {
+                case 0: stop = true; break;
+                case 90: stop = true; break;
+                case 180: stop = true; break;
+                case 270: stop = true; break;
+                default: stop = false; break;
+            }
+
+            if (stop && y_axis == Coordinate)
             {
                 rot_right = false;
                 rot_left = false;
                 rotated = false;
                 CancelInvoke("Spine");
             }
-        }
-        private void StopCheck()
-        {
 
         }
         #endregion
@@ -112,10 +130,10 @@ namespace solar_a
 
             if (!rotated)
             {
-                if (Input.GetAxisRaw("Left_Spine") == 1) { rot_left = true; }
-                else if (Input.GetAxisRaw("Right_Spine") == 1) { rot_right = true; }
+                rot_left = Input.GetAxisRaw("Left_Spine") == 1;
+                rot_right = Input.GetAxisRaw("Right_Spine") == 1;
+                DirectCheck();
             }
-            DirectCheck();
             //PositionCheck(-1);
         }
         #endregion
@@ -128,22 +146,35 @@ namespace solar_a
     迴轉流程：
     1. 按鍵判定：左、右鍵偵測
         按鍵測定的構思：按下按鍵後，僅只是啟動轉向判定，真正執行旋轉的判定應該要在位置判定之後，才不會按鍵影響旋轉。
+        誤區：
+        (1) 旋轉判定只能執行一次：如果放在更新事件中，會重複呼叫旋轉程式而出現不必要的錯誤。
+        (2) 將位置判定放在更新事件中或重複執行：如果會動到定點，則好不容易設定的定點就會跑掉。
+        (3) 切換左右轉怎麼換？
 
     2. 位置判定：目前在哪個座標上
+        這邊只有一個重點：「過軸轉象限。」
+        也許有更好的寫法，但如果使用角度計算的話，在0度要-90度的時候就要先將0轉成360度。
+        但這樣改也花費不少時間。
 
     3. 旋轉判定：一旦開始旋轉，沒有到目標點不會停
         旋轉方法：
         這次直接做旋轉的模式，至於甚麼時候旋轉，甚麼時候停止則不再這個程式中決定。
         需要用到的變數主要有 Y軸目前的位置 與 下一個座標點的位置。
         尤其下一個座標位置不能隨程式執行而變動，不然才剛抵達就切到下一個座標，就會轉不停。
+        速度曲線：
+        決定 iSpine 的大小除了手動調整 Spine 之外，重要的是 iSpine 的變化。
+        目前先做線性的設計，之後再觀察如何設定其他樣式。
+        可以確定的是，如果線性無法完美停止，其他方程式自然容易暴衝。
 
     4. 定點判定：旋轉是否抵達定點，抵達後結束程式
         到達定點的做法：
         (1) 讀取定點的位置
         (2) 取距離公式:Mathf.DeltaAngle
         (3) 如果小於一定值，有兩個做法，影響的層級不同
-            A. 停止   B. 卡上去
+            A. 停止   B. 速度遞減
+            目前採 B.
         (4) 回傳停止訊號 or 指定條件停止
+            採速度遞減的關係，所以可以指定條件停止
 
     PS: 函式跟變數的差異？
         函式可以傳入參數，也可以回傳參數，乍看之下其實跟變數相同，但是最大的差別就在函式只要呼叫就會執行內部的程式碼，變數只有在設定欄位時
