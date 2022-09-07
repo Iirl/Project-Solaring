f0eb4ad874c24fd97a67d7900e6aa5e1703234d2
Iirl <102848236+Iirl@users.noreply.github.com>
2022-09-07 23:15:08 +0800

    Simple Move Editorlize

--- Project-Solaring_mk2/Assets/Scripts/Stage/Simple_move.cs
+++ Project-Solaring_mk2/Assets/Scripts/Stage/Simple_move.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace solar_a
@@ -11,7 +12,7 @@ namespace solar_a
     /// </summary>
     public class Simple_move : MonoBehaviour
     {
-        enum MoveMethod { Straight, Track, Direction, Hold }
+        public enum MoveMethod { Straight, Track, Direction, Hold }
         #region 面板控制屬性
 
         #endregion
@@ -23,15 +24,15 @@ namespace solar_a
         /// 在面板顯示設定
         /// </summary>
         [SerializeField, Header("移動方式")]
-        private MoveMethod moveMethod;
-        [SerializeField, Header("直線移動方向")]
-        private Vector3 straightV3;
+        public MoveMethod moveMethod;
         [SerializeField, Header("移動速度"), Tooltip("Please test it until you want's speed.")]
-        private float Orispeed = 1.2f;
+        protected float Orispeed = 1.2f;
         [SerializeField, Tooltip("Set random speed to set every object have different speed.")]
-        private bool randomSpd = true;
-        [SerializeField, Header("停止追蹤距離"), Tooltip("If your Screen size less than 12, recommend to fix it.")]
-        private float stopTracert = 12;
+        protected bool randomSpd = true;
+        [SerializeField, Header("直線移動方向"),HideInInspector]
+        protected Vector3 straightV3;
+        [SerializeField, Header("停止追蹤距離"), Tooltip("If your Screen size less than 12, recommend to fix it."), HideInInspector]
+        protected float stopTracert = 12;
         private float dist;
         //
         private AudioSource[] audios;
@@ -43,13 +44,14 @@ namespace solar_a
         /// <summary>
         /// 直線移動方法：在面板中設定移動方向
         /// </summary>
-        private void StraightMove() => transform.Translate(straightV3* Orispeed * Time.deltaTime, Space.World);
+        private void StraightMove() => transform.Translate(straightV3 * Orispeed * Time.deltaTime, Space.World);
         /// <summary>
         /// 直線移動方法：依據 dist 和 target 的參數決定移動。
         /// * 停止追蹤：設定當 dist 小於一定值之後就不更新 direct 和 target資訊。
         /// </summary>
         private void TransMove()
         {
+            if (target == null) target = GameObject.FindWithTag("Player");
             transform.LookAt(target.transform.position);
             dist = Vector3.Distance(transform.position, target_v3);
             float speed = Orispeed / dist * Time.deltaTime;
@@ -89,9 +91,9 @@ namespace solar_a
         }
         void Start()
         {
-            target_v3 = target ? target.transform.position: straightV3;                // 設定目標的座標
+            target_v3 = target ? target.transform.position : straightV3;                // 設定目標的座標
             direct = (transform.position - target_v3).normalized; // 設定目標的方向
-            if (randomSpd) Orispeed = Random.Range(Orispeed, Orispeed*2);
+            if (randomSpd) Orispeed = Random.Range(Orispeed, Orispeed * 2);
         }
 
         // Update is called once per frame
@@ -108,7 +110,7 @@ namespace solar_a
                 case MoveMethod.Direction:
                     ContinueMove();
                     break;
-                case MoveMethod.Hold:                    
+                case MoveMethod.Hold:
                     break;
                 default:
                     break;
@@ -118,8 +120,46 @@ namespace solar_a
         private void OnTriggerEnter(Collider other)
         {
             //print("碰到物件");
-            if (other.tag.Contains("Player")) if(!ManageCenter.space_ctl.isRotate) enabled = false;
+            if (other.tag.Contains("Player")) if (!ManageCenter.space_ctl.isRotate) enabled = false;
         }
         #endregion
+
+
+        [CustomEditor(typeof(Simple_move))]
+        public class ListMoveEditor : Editor
+        {
+            SerializedProperty spMethod;
+            SerializedProperty spStraight;
+            SerializedProperty spStopTracert;
+
+            public override void OnInspectorGUI()
+            {
+                DrawDefaultInspector();
+                serializedObject.Update();
+                spMethod = serializedObject.FindProperty("moveMethod");
+                spStraight = serializedObject.FindProperty("straightV3");
+                spStopTracert = serializedObject.FindProperty("stopTracert");
+
+                if (spMethod.enumValueFlag == 0)
+                {
+                    EditorGUILayout.PropertyField(spStraight);
+
+                }
+                else if (spMethod.enumValueFlag == 1)
+                {
+
+                }
+                else if (spMethod.enumValueFlag == 2)
+                {
+                    EditorGUILayout.PropertyField(spStopTracert);
+                }
+                else if (spMethod.enumValueFlag == 3)
+                {
+
+                }
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
     }
 }
