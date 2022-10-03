f58e4fbbd0f356d87ecf3a31a2490aad7f7a6fa7
Iirl <102848236+Iirl@users.noreply.github.com>
2022-10-03 13:39:38 +0800

    bug修正

---
+++ Project-Solaring_mk2/Assets/Scripts/Other/TimeFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFunction : MonoBehaviour
+{
+	[SerializeField]
+	KeyCode TimeSpeedKey;
+	
+	private void TimeSpeed(KeyCode input, float t = 3){
+		if (Input.GetKey(input)) Time.timeScale = t;
+		else Time.timeScale =1;
+	}
+	protected void Update()
+	{
+		TimeSpeed(TimeSpeedKey);
+	}
+}
